defd72729b70e439d48a903105027e308ddf3cc8
Kman <krish9pro@gmail.com>
2026-09-21 13:17:18 +0530

    some fixes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from src.components.classification.model_evaluation import ModelEvaluation
 
 from src.components.segmentation.data_transformation import DataTransformation as Transformer
 from src.components.segmentation.model_trainer import ModelTrainer as Trainer
+from src.components.segmentation.model_evaluation import ModelEvaluation as Evaluator
 
 le = LabelEncoder()
 
@@ -67,26 +68,29 @@ if __name__ == "__main__":
     classification_transformation_obj = DataTransformation(classification_config, train_df, test_df, val_df, le)
     classification_train, classification_test, classification_val, class_map = classification_transformation_obj.initiate_transformation()
 
-    # model training
-    model_trainer_obj = ModelTrainer(model_trainer_config, classification_train, classification_val)
-    model_trainer_obj.train()
+    # # model training
+    # model_trainer_obj = ModelTrainer(model_trainer_config, classification_train, classification_val)
+    # model_trainer_obj.train()
 
     # model evaluation
     model_eval_obj = ModelEvaluation(evlaution, classification_test, class_map)
     model_eval_obj.evaluate()
 
-    # # data transformation
-    # train_s = manifest_path / 'segmentation' / 'train.csv'
-    # test_s = manifest_path / 'segmentation' / 'test.csv'
-    # val_s = manifest_path / 'segmentation' / 'val.csv'
+    # data transformation
+    train_s = manifest_path / 'segmentation' / 'train.csv'
+    test_s = manifest_path / 'segmentation' / 'test.csv'
+    val_s = manifest_path / 'segmentation' / 'val.csv'
 
-    # train_df_s = pd.read_csv(train_s)
-    # test_df_s = pd.read_csv(test_s)
-    # val_df_s = pd.read_csv(val_s)
+    train_df_s = pd.read_csv(train_s)
+    test_df_s = pd.read_csv(test_s)
+    val_df_s = pd.read_csv(val_s)
 
-    # segmentation_transformation_obj = Transformer(config = segmentation_config, train = train_df_s, test = test_df_s, val = val_df_s)
-    # segmentation_train, segmentation_test, segmentation_val = segmentation_transformation_obj.initiate_transformation()
+    segmentation_transformation_obj = Transformer(config = segmentation_config, train = train_df_s, test = test_df_s, val = val_df_s)
+    segmentation_train, segmentation_test, segmentation_val = segmentation_transformation_obj.initiate_transformation()
 
     # # model training
     # model_trainer_obj = Trainer(model_trainer_config, segmentation_train, segmentation_val)
     # model_trainer_obj.train()
+
+    # model evaluation
+    model_eval_obj = Evaluator(evlaution, test_loader = segmentation_test)
